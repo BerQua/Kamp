5b6c42cf31c0ce6c5d1b49b8239e238e4b376340
Berk <116597451+BerQua@users.noreply.github.com>
2023-03-11 19:12:06 +0300

    Add files via upload

---
+++ Python-Selenium Kamp/Dersler/gun1.py
@@ -0,0 +1,16 @@
+print("Kodlamio")
+baslik = "Taşıt Kredisi"
+print(baslik)
+#string => metinsel ifade
+baslik = "İhtiyaç Kredisi"
+print(baslik)
+#int, integer => tam sayı
+vade = 36
+vade2 = "36"
+
+#float, decimal, double
+aylikOdeme = 200.0
+
+#bool, boolean => True veya False
+yukselistemi = False
+
